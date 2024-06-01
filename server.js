8bf92a5a7bb770e9db44adb012105a98da5738b0
gedemartha <alphamart12@gmail.com>
2024-06-01 16:02:43 +0800

    updated stuff

--- server.js
+++ server.js
@@ -11,19 +11,19 @@ app.get("/", (req, res) => {
   console.log("get response");
   //   res.sendStatus(500); // send status code ... 200 is OK, 201 is Sent, 400 is client error, 500 is server error.
 
-  //   res.status(500).send("Hi"); // passing error message
-  //   res.status(500).json({ message: "Error" }); // passing error message in json
+  //   res.status(500).send("Hi");                          // passing error message
+  //   res.status(500).json({ message: "Error" });          // passing error message in json
 
-  //   res.download(/*file path*/) // download file from server to client
+  //   res.download(/*file path*/)                          // download file from server to client
 
-  res.render("index"); // render HTML
+  res.render("index");                                      // render HTML
 
-  //   res.send("Hi (get response)");
+  //res.send("Hi (get response)"); 
 });
 
 const userRouter = require("./routes/users");
 
-app.use("/users", userRouter);
+app.use("/users", userRouter); 
 
 // function logger(req, res, next) { // middleware
 //   console.log(req.originalURL);
